a2ff5ecec019a1d4ccb7439aa24f7c04ab1f906c
Sebastian <sebastian.hegmann@hs-augsburg.de>
2018-04-24 23:49:24 +0200

    Code Umstrukturierung

--- EX1/exercise1.py
+++ EX1/exercise1.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.ndimage as ndi
 import skimage.feature as feat
 
+
 def load_image(path):
     file = h5py.File(path, 'r')
     if file != None:
@@ -12,15 +13,21 @@ def load_image(path):
     print("File can not be loaded")
 
 def visualize_images(imDsets, num):
-    plt.figure(1, figsize=(20,20))
+    global count
+    plt.figure(count, figsize=(20,20))
+    count = count + 1
+
     for i in range(num):
         plt.subplot(3,(num//3)+1,i+1)
         plt.title('image '+str(i+1), fontsize=10)
         plt.imshow(imDsets[i])
+
     plt.suptitle('Images and their related labels', fontsize=30)
-    plt.show()
+    
+    
 
 if __name__ == "__main__":
+    count = 0
     images1 = load_image("images_subject02.h5")
     images2 = load_image("images_subject10.h5")
     labels1 = load_image("labels_subject02.h5")
@@ -31,24 +38,18 @@ if __name__ == "__main__":
     keys_labels1 = list(labels1.keys())
     keys_labels2 = list(labels2.keys())
     
-    plt.figure(1, figsize=(20,20))
-    indexes = [1,3,5,2,4,6]
+    plt.figure(count, figsize=(20,20))
+    count = count + 1
+    indices = [1,3,5,2,4,6]
     imDsets = []
     lblDsets = []
+
     for i in range(3):
         imDsets.append(images1[keys_images1[i]])
         lblDsets.append(labels1[keys_labels1[i]])
-        plt.subplot(3,2,indexes[i])
-        plt.title('image '+str(i+1), fontsize=20)
-        plt.imshow(imDsets[i])
-        plt.subplot(3,2,indexes[i+3])
-        plt.title('label '+str(i+1), fontsize=20)
-        plt.imshow(lblDsets[i])
-
-
-    plt.suptitle('Images and their related labels', fontsize=30)
-    plt.show()
-
+    
+    visualize_images(imDsets, len(imDsets))
+ 
     sigma=[0.7, 1, 1.6, 3.5, 5, 10]
     gauss=[]
     gaussLaplace=[]
@@ -70,8 +71,10 @@ if __name__ == "__main__":
     square = np.zeros((5, 5))
     square[2, 2] = 4
     H_elems = feat.hessian_matrix(square, sigma=0.1, order='rc')
-    feat.hessian_matrix_eigvals(H_elems)[0]
+    #feat.hessian_matrix_eigvals(H_elems)[0]
 
     visualize_images(gauss, len(gauss))
     visualize_images(gaussLaplace, len(gaussLaplace))
     visualize_images(gaussGradientMagnitude, len(gaussGradientMagnitude))
+
+    plt.show()
