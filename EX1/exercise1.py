0bd706400a7114392a08b28a80e36c52994e6f64
paulhfu <hiltpa@gmail.com>
2018-04-24 16:29:28 +0200

    1. task visualize the data

--- EX1/exercise1.py
+++ EX1/exercise1.py
@@ -12,14 +12,26 @@ def load_image(path):
 if __name__ == "__main__":
     images1 = load_image("images_subject02.h5")
     images2 = load_image("images_subject10.h5")
-    labels02 = load_image("labels_subject02.h5")
-    labels10 = load_image("labels_subject10.h5")
+    labels1 = load_image("labels_subject02.h5")
+    labels2 = load_image("labels_subject10.h5")
     
     keys_images1 = list(images1.keys())
     keys_images2 = list(images2.keys())
+    keys_labels1 = list(labels1.keys())
+    keys_labels2 = list(labels2.keys())
     
+    plt.figure(1, figsize=(20,20))
+    indexes = [1,3,5,2,4,6]
+    imDsets = []
+    lblDsets = []
     for i in range(3):
-        dset = images1[keys_images1[i]]
-        plt.figure()
-        plt.imshow(dset)
-        plt.show()
+        imDsets.append(images1[keys_images1[i]])
+        lblDsets.append(labels1[keys_labels1[i]])
+        plt.subplot(3,2,indexes[i])
+        plt.title('image '+str(i+1), fontsize=20)
+        plt.imshow(imDsets[i])
+        plt.subplot(3,2,indexes[i+3])
+        plt.title('label '+str(i+1), fontsize=20)
+        plt.imshow(lblDsets[i])
+    plt.suptitle('Images and their related labels', fontsize=30)
+    plt.show()
