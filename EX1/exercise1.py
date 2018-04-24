64740770e1f13f00423a4105334757f8827e5087
paulhfu <hiltpa@gmail.com>
2018-04-24 18:52:41 +0200

    ex 1.1

--- EX1/exercise1.py
+++ EX1/exercise1.py
@@ -1,6 +1,8 @@
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage as ndi
+import skimage.feature as feat
 
 def load_image(path):
     file = h5py.File(path, 'r')
@@ -8,6 +10,15 @@ def load_image(path):
         return file
 
     print("File can not be loaded")
+
+def visualize_images(imDsets, num):
+    plt.figure(1, figsize=(20,20))
+    for i in range(num):
+        plt.subplot(3,(num//3)+1,i+1)
+        plt.title('image '+str(i+1), fontsize=10)
+        plt.imshow(imDsets[i])
+    plt.suptitle('Images and their related labels', fontsize=30)
+    plt.show()
 
 if __name__ == "__main__":
     images1 = load_image("images_subject02.h5")
@@ -37,3 +48,30 @@ if __name__ == "__main__":
 
     plt.suptitle('Images and their related labels', fontsize=30)
     plt.show()
+
+    sigma=[0.7, 1, 1.6, 3.5, 5, 10]
+    gauss=[]
+    gaussLaplace=[]
+    gaussGradientMagnitude=[]
+
+    structTensor=[]
+    hessEigenvalues=[]
+    for i in range(3):
+        for j in range(len(sigma)):
+            gauss.append(ndi.gaussian_filter(imDsets[i], sigma[j]))
+            gaussLaplace.append(ndi.gaussian_laplace(imDsets[i], sigma[j]))
+            gaussGradientMagnitude.append(ndi.gaussian_gradient_magnitude(imDsets[i], sigma[j]))
+
+            structTensor.append(feat.structure_tensor(imDsets[i], sigma[j]))
+            hessianMat = feat.hessian_matrix(imDsets[i], sigma[j])
+            #feat.hessian_matrix_eigvals(hessianMat, Hxy=None, Hyy=None)
+            #hessEigenvalues.append(tuple(hxy, hyy, hxx))
+
+    square = np.zeros((5, 5))
+    square[2, 2] = 4
+    H_elems = feat.hessian_matrix(square, sigma=0.1, order='rc')
+    feat.hessian_matrix_eigvals(H_elems)[0]
+
+    visualize_images(gauss, len(gauss))
+    visualize_images(gaussLaplace, len(gaussLaplace))
+    visualize_images(gaussGradientMagnitude, len(gaussGradientMagnitude))
