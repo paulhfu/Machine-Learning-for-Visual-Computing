44b1001752f3176a4cead52f201f29fd7197d4a3
paulhfu <hiltpa@gmail.com>
2018-04-25 22:06:22 +0200

    final

--- EX1/exercise1.py
+++ EX1/exercise1.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.ndimage as ndi
 import skimage.feature as feat
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 
 
 def load_image(path):
@@ -21,8 +23,6 @@ def visualize_imagesAndLables(imDsets, lblDsets):
     indices = [1, 3, 5, 2, 4, 6]
 
     for i in range(3):
-        imDsets.append(images1[keys_images1[i]])
-        lblDsets.append(labels1[keys_labels1[i]])
         plt.subplot(3,2,indices[i])
         plt.title('image '+str(i+1), fontsize=20)
         plt.imshow(imDsets[i])
@@ -44,11 +44,6 @@ def apply_filters(imDsets, lblDsets, sigma, gauss, gaussLaplace, gaussGradientMa
             hessianMat = feat.hessian_matrix(imDsets[i], sigma[j])
             #feat.hessian_matrix_eigvals(hessianMat, Hxy=None, Hyy=None)
             #hessEigenvalues.append(tuple(hxy, hyy, hxx))
-
-    square = np.zeros((5, 5))
-    square[2, 2] = 4
-    H_elems = feat.hessian_matrix(square, sigma=0.1, order='rc')
-    #feat.hessian_matrix_eigvals(H_elems)[0]
 
 
 def visualize_filters():
@@ -89,6 +84,10 @@ if __name__ == "__main__":
     imDsets = []
     lblDsets = []
 
+    for i in range(3):
+        imDsets.append(images1[keys_images1[i]])
+        lblDsets.append(labels1[keys_labels1[i]])
+
     sigma = [0.7, 1, 1.6, 3.5, 5, 10]
 
     gauss = []
@@ -98,8 +97,35 @@ if __name__ == "__main__":
     structTensor = []
     hessEigenvalues = []
 
-    visualize_imagesAndLables(imDsets, lblDsets)
+#    visualize_imagesAndLables(imDsets, lblDsets)
     apply_filters(imDsets, lblDsets, sigma, gauss, gaussLaplace, gaussGradientMagnitude, structTensor, hessEigenvalues)
-    visualize_filters()
+#    visualize_filters()
 
-    plt.show()
+    ngauss=np.array(gauss)
+    ngaussLaplace=np.array(gaussLaplace)
+    ngaussGradientMagnitude=np.array(gaussGradientMagnitude)
+    nstructTensor=np.array(structTensor)
+    nlblDsets=np.array(lblDsets)
+    print(nstructTensor.shape)
+    input=np.zeros((36,3*768*496))
+
+    for i in range(6):
+        input[i][:]=np.append(ngauss[i].reshape((1,len(ngauss[0])*len(ngauss[0][0]))), [ngauss[i+6].reshape((1,len(ngauss[0])*len(ngauss[0][0]))), ngauss[i+12].reshape((1,len(ngauss[0])*len(ngauss[0][0])))])
+    for i in range(6):
+        input[i+6][:]=np.append(ngaussLaplace[i].reshape((1,len(ngaussLaplace[0])*len(ngaussLaplace[0][0]))), [ngaussLaplace[i+6].reshape((1,len(ngaussLaplace[0])*len(ngaussLaplace[0][0]))), ngaussLaplace[i+12].reshape((1,len(ngaussLaplace[0])*len(ngaussLaplace[0][0])))])
+    for i in range(6):
+        input[i+12][:]=np.append(ngaussGradientMagnitude[i].reshape((1,len(ngaussGradientMagnitude[0])*len(ngaussGradientMagnitude[0][0]))), [ngaussGradientMagnitude[i+6].reshape((1,len(ngaussGradientMagnitude[0])*len(ngaussGradientMagnitude[0][0]))), ngaussGradientMagnitude[i+12].reshape((1,len(ngaussGradientMagnitude[0])*len(ngaussGradientMagnitude[0][0])))])
+    for j in range(3):
+        for i in range(6):
+            input[i][:]=np.append(nstructTensor[i][j].reshape((1,len(nstructTensor[0][0])*len(nstructTensor[0][0][0]))), [nstructTensor[i+6][j].reshape((1,len(nstructTensor[0][0])*len(nstructTensor[0][0][0]))), nstructTensor[i+12][j].reshape((1,len(nstructTensor[0][0])*len(nstructTensor[0][0][0])))])
+
+    input=np.swapaxes(input, 0, 1)
+    output=np.append(nlblDsets[0].reshape((1,len(nlblDsets[0])*len(nlblDsets[0][0]))), [nlblDsets[1].reshape((1,len(nlblDsets[0])*len(nlblDsets[0][0]))), nlblDsets[2].reshape((1,len(nlblDsets[0])*len(nlblDsets[0][0])))])
+    
+    rf = RandomForestClassifier()
+    rf.fit(input,output)
+    inputProps=rf.predict_proba(input)
+
+    hf = h5py.File('propsData.h5','w')
+    create_dataset('inputProps', data=inputProps)
+    close()
