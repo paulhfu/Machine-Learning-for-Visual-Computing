12cc1afb05923328e34ab8e522527c1137a3ff09
Sebastian <sebastian.hegmann@hs-augsburg.de>
2018-05-14 00:53:10 +0200

    Exercise 2 - shortest path

--- EX4/EX4.py
+++ EX4/EX4.py
@@ -1,7 +1,77 @@
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+from random import randint
+
+
+def initUnaryProbabilities():
+    for i in range(len(predictions)):
+        for j in range(len(predictions[0])):
+            predictions[i][j] = randint(1, classes + 1)
+
+
+def createPairProbMatrix():
+
+    return np.matrix([[0,   1,   5], 
+                      [100, 0,   1],
+                      [100, 100, 0]])
+
+
+def absorbUnaryPot():
+    unaryOnes = []
+    onesUnary = []
+    for i in range(classes):
+        unaryOnes.append(np.multiply(predictions[i][:], np.ones((classes, 1))).transpose())
+        onesUnary.append(np.multiply(np.ones((classes, 1)), predictions[i][:]))
+        
+    potential = []
+    for j in range(classes - 1):
+        if(j != classes - 2):
+            potential.append(pairProp + unaryOnes[j] + onesUnary[j + 1])
+        else:
+            potential.append(pairProp + onesUnary[j + 1])
+
+    return np.array(potential)
+
+
+def updatePotentials(index):
+    if len(minimas) == 0:
+        return 0
+    else:
+        for i in range(classes):
+            for j in range(classes):
+                potentials[index][j][i] = potentials[index][j][i] + minimas[i - classes]
+
+    return 1
+
+
+def calculateMinima():
+    index = (int)(len(minimas) / classes)
+    updatePotentials(index)
+
+    for i in range(classes):
+        minimas.append(np.amin(potentials[index][:, i]))
+
+
+def calculateShortestPath():
+
+    for i in range(classes - 1):
+        calculateMinima()
+
 
 
 if __name__ == '__main__':
-    
+    classes = 3
+    predictions = np.zeros((classes, classes))
+
+    initUnaryProbabilities()
+    pairProp = createPairProbMatrix()
+
+    potentials = absorbUnaryPot()
+
+    shortestPath = []
+    minimas = []
+    costs = []
+
+    calculateShortestPath()
+    print("Costs: ", minimas[-classes:])
